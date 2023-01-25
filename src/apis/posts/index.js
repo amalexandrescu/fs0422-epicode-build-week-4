77108104031a88c6c3419860129a3ee2fe019ebe
Daniel <dan.earp@icloud.com>
2023-01-25 19:01:24 +0000

    Update index.js

--- src/apis/posts/index.js
+++ src/apis/posts/index.js
@@ -1,124 +1,124 @@
-import express from "express";
-import { model } from "mongoose";
-import posts from "./model.js";
-import createHttpError from "http-errors";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import multer from "multer";
-import { v2 as cloudinary } from "cloudinary";
-import UsersModel from "../users/model.js";
+import express from 'express'
+import { model } from 'mongoose'
+import posts from './model.js'
+import createHttpError from 'http-errors'
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
+import multer from 'multer'
+import { v2 as cloudinary } from 'cloudinary'
+import UsersModel from '../users/model.js'
 
-const postsRouter = express.Router();
+const postsRouter = express.Router()
 
 const postCoverPhoto = multer({
   storage: new CloudinaryStorage({
     cloudinary,
     params: {
-      folder: "LinkedIn",
+      folder: 'LinkedIn',
     },
   }),
-}).single("postPhoto");
+}).single('postPhoto')
 
 //Add a new post
-postsRouter.post("/", async (request, response, next) => {
+postsRouter.post('/', async (request, response, next) => {
   try {
-    const newPost = new posts(request.body);
-    const { _id } = await newPost.save();
-    response.status(200).send({ _id });
+    const newPost = new posts(request.body)
+    const { _id } = await newPost.save()
+    response.status(200).send({ _id })
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 //Get all posts
-postsRouter.get("/", async (request, response, next) => {
+postsRouter.get('/', async (request, response, next) => {
   try {
     const getPosts = await posts
       .find({})
-      .populate({ path: "user", select: "name surname image" });
-    response.status(200).send(getPosts);
+      .populate({ path: 'user', select: 'name surname image' })
+    response.status(200).send(getPosts)
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
 //Get post by ID
-postsRouter.get("/:postID", async (request, response, next) => {
+postsRouter.get('/:postID', async (request, response, next) => {
   try {
-    const getPost = await posts.findById(request.params.postID);
+    const getPost = await posts.findById(request.params.postID)
 
     if (getPost) {
-      response.status(200).send(getPost);
+      response.status(200).send(getPost)
     } else {
       next(
         createHttpError(
           404,
-          `Unable to find post with ID ${request.params.postID}`
-        )
-      );
+          `Unable to find post with ID ${request.params.postID}`,
+        ),
+      )
     }
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
 //Edit a post
-postsRouter.put("/:postID", async (request, response, next) => {
+postsRouter.put('/:postID', async (request, response, next) => {
   try {
     const post = await posts.findByIdAndUpdate(
       request.params.postID,
       request.body,
-      { new: true }
-    );
-    response.status(200).send(post);
+      { new: true },
+    )
+    response.status(200).send(post)
   } catch (error) {
-    next(err);
+    next(err)
   }
-});
+})
 
 //Delete a post
-postsRouter.delete("/:postID", async (request, response, next) => {
+postsRouter.delete('/:postID', async (request, response, next) => {
   try {
-    const post = await posts.findByIdAndDelete(request.params.postID);
+    const post = await posts.findByIdAndDelete(request.params.postID)
 
     if (post) {
-      response.status(204).send();
+      response.status(204).send()
     } else {
       next(
         createHttpError(
           404,
-          `There was no post with the ID ${request.params.postID}. Nothing has been deleted.`
-        )
-      );
+          `There was no post with the ID ${request.params.postID}. Nothing has been deleted.`,
+        ),
+      )
     }
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
 //Upload an image and attach it to a post
 postsRouter.post(
-  "/:postID/uploadPostImage",
+  '/:postID/uploadPostImage',
   postCoverPhoto,
   async (request, response, next) => {
     try {
-      const url = request.file.path;
+      const url = request.file.path
 
       const post = await posts.findByIdAndUpdate(request.params.postID, {
         image: url,
-      });
-      response.status(200).send("Image uploaded");
+      })
+      response.status(200).send('Image uploaded')
     } catch (err) {
-      next(err);
+      next(err)
     }
-  }
-);
+  },
+)
 
 // *************************** COMMENTS ***************************
 
-postsRouter.post("/:postId/comment", async (req, res, next) => {
+postsRouter.post('/:postId/comment', async (req, res, next) => {
   //in req.body we pot the comment and the user id
   try {
-    const searchedPost = await posts.findById(req.params.postId);
+    const searchedPost = await posts.findById(req.params.postId).limit(2)
 
     if (searchedPost) {
       const updatedPost = await posts.findByIdAndUpdate(
@@ -126,143 +126,143 @@ postsRouter.post("/:postId/comment", async (req, res, next) => {
         {
           $push: { comments: { ...req.body } },
         },
-        { new: true, runValidators: true }
-      );
-      res.send(updatedPost);
+        { new: true, runValidators: true },
+      )
+      res.send(updatedPost)
     } else {
-      next(createHttpError(404, `Post with id ${req.params.postId} not found`));
+      next(createHttpError(404, `Post with id ${req.params.postId} not found`))
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-postsRouter.get("/:postId/comment", async (req, res, next) => {
+postsRouter.get('/:postId/comment', async (req, res, next) => {
   try {
     const currentPost = await posts
       .findById(req.params.postId)
-      .populate({ path: "comments.userId", select: "name surname image" });
+      .populate({ path: 'comments.userId', select: 'name surname image' })
     if (currentPost) {
-      res.send(currentPost.comments);
+      res.send(currentPost.comments)
     } else {
-      next(createHttpError(404, `Post with id ${req.params.postId} not found`));
+      next(createHttpError(404, `Post with id ${req.params.postId} not found`))
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-postsRouter.delete("/:postId/comment/:commentId", async (req, res, next) => {
+postsRouter.delete('/:postId/comment/:commentId', async (req, res, next) => {
   try {
     const updatedPost = await posts.findByIdAndUpdate(
       req.params.postId,
       { $pull: { comments: { _id: req.params.commentId } } },
-      { new: true }
-    );
+      { new: true },
+    )
     if (updatedPost) {
-      res.send(updatedPost);
+      res.send(updatedPost)
     } else {
-      next(createHttpError(404, `Post with id ${req.params.postId} not found`));
+      next(createHttpError(404, `Post with id ${req.params.postId} not found`))
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-postsRouter.put("/:postId/comment/:commentId", async (req, res, next) => {
+postsRouter.put('/:postId/comment/:commentId', async (req, res, next) => {
   try {
-    const searchedPost = await posts.findById(req.params.postId);
+    const searchedPost = await posts.findById(req.params.postId)
 
     if (searchedPost) {
       const index = searchedPost.comments.findIndex(
-        (comment) => comment._id.toString() === req.params.commentId
-      );
+        (comment) => comment._id.toString() === req.params.commentId,
+      )
 
       if (index !== -1) {
         searchedPost.comments[index] = {
           ...searchedPost.comments[index].toObject(),
           ...req.body,
-        };
-        await searchedPost.save();
-        res.send(searchedPost);
+        }
+        await searchedPost.save()
+        res.send(searchedPost)
       } else {
         next(
           createHttpError(
             404,
-            `Comment with id ${req.params.commentId} not found`
-          )
-        );
+            `Comment with id ${req.params.commentId} not found`,
+          ),
+        )
       }
     } else {
-      next(createHttpError(404, `Post with id ${req.params.postId} not found`));
+      next(createHttpError(404, `Post with id ${req.params.postId} not found`))
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 // *************************** LIKES ***************************
 
-postsRouter.post("/:postId/like", async (req, res, next) => {
+postsRouter.post('/:postId/like', async (req, res, next) => {
   try {
     //in the req.body we'll get the userId;
 
-    const { userId } = req.body;
+    const { userId } = req.body
 
-    const post = await posts.findById(req.params.postId);
+    const post = await posts.findById(req.params.postId)
 
     if (!post) {
       return next(
-        createHttpError(404, `Post with id ${req.params.postId} not found`)
-      );
+        createHttpError(404, `Post with id ${req.params.postId} not found`),
+      )
     }
 
-    const user = await UsersModel.findById(userId);
+    const user = await UsersModel.findById(userId)
 
     if (!user) {
-      return next(createHttpError(404, `User with id ${userId} not found`));
+      return next(createHttpError(404, `User with id ${userId} not found`))
     }
 
     const checkLike = post.likes.findIndex(
-      (like) => like.toString() === userId.toString()
-    );
+      (like) => like.toString() === userId.toString(),
+    )
 
     if (checkLike === -1) {
       const modifiedPost = await posts
         .findOneAndUpdate(
           post,
           { $push: { likes: userId } },
-          { new: true, runValidators: true }
+          { new: true, runValidators: true },
         )
-        .populate({ path: "likes", select: "name surname image" });
+        .populate({ path: 'likes', select: 'name surname image' })
 
-      res.send(modifiedPost);
+      res.send(modifiedPost)
     } else {
-      next(createHttpError(400, `User already liked that post`));
+      next(createHttpError(400, `User already liked that post`))
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-postsRouter.delete("/:postId/like", async (req, res, next) => {
+postsRouter.delete('/:postId/like', async (req, res, next) => {
   try {
-    const { userId } = req.body;
+    const { userId } = req.body
     const updatedPost = await posts
       .findByIdAndUpdate(
         req.params.postId,
         { $pull: { likes: userId } },
-        { new: true }
+        { new: true },
       )
-      .populate({ path: "likes", select: "name surname image" });
+      .populate({ path: 'likes', select: 'name surname image' })
     if (updatedPost) {
-      res.send(updatedPost);
+      res.send(updatedPost)
     } else {
-      next(createHttpError(404, `Post with id ${req.params.postId} not found`));
+      next(createHttpError(404, `Post with id ${req.params.postId} not found`))
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-export default postsRouter;
+export default postsRouter
